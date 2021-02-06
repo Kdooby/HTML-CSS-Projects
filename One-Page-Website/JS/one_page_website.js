1dc98c8d47a8b6b41b366cd783e1b85b3e47b4f7
Kdooby <kevinmdem@gmail.com>
2021-02-05 16:52:27 -0800

    Commit Projects

---
+++ One-Page-Website/JS/one_page_website.js
@@ -0,0 +1,65 @@
+// GALLERY MODAL
+
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
+}
+
+function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    captionText.innerHTML = dots[slideIndex - 1].alt;
+    
+}
+
+
+// END GALLERY MODAL
+
+//FORM VALIDATION
+
+function validateForm() {
+    var x = document.forms["sign_up"]["fname"].value;
+    if (x == "") {
+        alert("All sections must be filled out!");
+        return false;
+    }
+}
+
+//Opens and Closes the Contact Form
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
